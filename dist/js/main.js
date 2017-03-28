b16552d87f38cb767c1d73a3fb4e0f9d164d688c
saynschuman <saynschuman@gmail.com>
2017-03-28 20:29:24 +0300

    finished

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,24 @@
 (function ($) {
 
+	$('.toggle-input').focus(function(){
+		var id = $(this).attr('id');
+		$('ul[data-id="' + id + '"]').show();
+	})
+	$('.toggle-input').blur(function(){
+		var id = $(this).attr('id');
+		setTimeout(function(){
+			$('ul[data-id="' + id + '"]').hide();  
+		}, 200);
+		
+	})
+	$('.toggle-list li').click(function(){
+		var content = $(this).html(),
+			id = $(this).parent().attr('data-id');
+		
+		$('#' + id).val(content);
 
+		
+		
+	})
 
 }(jQuery));
